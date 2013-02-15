8b7fb09a1b78e0a1840939f6a26306312ea63696
Conrad Twizzle <conrad.twizzle@gmail.com>
2013-02-15 03:38:27 -0800

    updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
       end
 
       @follower = UserFollower.new(:follower => session[:user_id])
-      @follower.follows => follow_id
+      @follower.follows = follow_id
       if @follower.save
         render :json => true
       else
